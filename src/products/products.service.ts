eab6fb4d7493e388c1359da2a372520df1cdeae2
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-08 19:04:05 +0900

    Add product stock functions

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,6 +3,7 @@ import { Product } from './models/Product.model';
 import { mockProductData } from 'src/__mocks__/productData';
 import { AllowedDepartments } from './models/AllowedDepartments';
 import { Cart } from 'src/cart/models/Cart.model';
+import { OrderItem } from 'src/order/models/OrderItem';
 
 @Injectable()
 export class ProductsService {
@@ -18,15 +19,49 @@ export class ProductsService {
     return mockProductData.filter((product) => product.dept === dept);
   }
 
-  confirmProductStock(cart: Cart) {}
-
-  adjustProductStockOnOrder(cart: Cart) {
-    for (const orderedProduct of cart.productArr) {
-      const stockProduct = mockProductData.find(
-        (product) => product.SKU === orderedProduct.item_SKU,
-      );
-
-      stockProduct.stock -= orderedProduct.quantity;
+  confirmProductStock(orderItems: OrderItem[]) {
+    for (const item of orderItems) {
+      const product = this.findBySearchName(item.searchName);
+      if (!product.stock) {
+        return {
+          error: true,
+          errorMsg: 'Out of stock.',
+          errorItem: product.searchName,
+        };
+      }
+      if (product.stock < item.qty) {
+        return {
+          error: true,
+          errorMsg:
+            'Insufficient stock. Please reduce item quantity and try again.',
+          errorItem: product.searchName,
+        };
+      }
     }
+    return { error: false, errorMsg: '', errorItem: null };
+  }
+
+  adjustProductStockOnOrder(orderItems: OrderItem[]) {
+    for (const item of orderItems) {
+      const product = this.findBySearchName(item.searchName);
+      //Combine with top function logic
+      if (!product.stock) {
+        return {
+          error: true,
+          errorMsg: 'Out of stock.',
+          errorItem: product.searchName,
+        };
+      }
+      if (product.stock < item.qty) {
+        return {
+          error: true,
+          errorMsg:
+            'Insufficient stock. Please reduce item quantity and try again.',
+          errorItem: product.searchName,
+        };
+      }
+      product.stock -= item.qty;
+    }
+    return { error: false, errorMsg: '', errorItem: null };
   }
 }
