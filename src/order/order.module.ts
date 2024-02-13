c835cd470e1926933f7e4d475b97914325aa694a
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-13 17:36:28 +0900

    Import DatabaseModule

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderResolver } from './order.resolver';
 import { OrderService } from './order.service';
 import { ProductsService } from 'src/products/products.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [OrderResolver, OrderService, ProductsService],
 })
 export class OrderModule {}
