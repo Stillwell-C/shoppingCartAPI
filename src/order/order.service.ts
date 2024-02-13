203d0e464e8b7db6eb73cd90b04c5b3c399c4d87
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-13 18:39:16 +0900

    Add createOrder function

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Order, Prisma } from '@prisma/client';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
-export class OrderService {}
+export class OrderService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async createOrder(
+    orderData: Prisma.OrderCreateInput,
+    itemData: Prisma.OrderItemCreateInput[],
+  ): Promise<Order> {
+    return this.databaseService.order.create({
+      data: {
+        ...orderData,
+        orderItems: {
+          createMany: {
+            data: itemData,
+          },
+        },
+      },
+    });
+  }
+}
