9216f2746054c5211938087ad7116cccc8271a25
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-13 17:32:00 +0900

    Import DatabseModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductsResolver } from './products.resolver';
 import { ProductsService } from './products.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [ProductsResolver, ProductsService],
 })
 export class ProductsModule {}
