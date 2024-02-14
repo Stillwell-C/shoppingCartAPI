71c0ed592195d9e98086ceb5a01892b9046d6b47
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-14 12:08:11 +0900

    Add all returnable fields

--- src/order/models/FindOrderReturnData.ts
+++ src/order/models/FindOrderReturnData.ts
@@ -1,4 +1,4 @@
-import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Field, Float, ID, ObjectType } from '@nestjs/graphql';
 import { OrderStatus } from '@prisma/client';
 import { FindOrderOrderItem } from './FindOrderOrderItem';
 
@@ -8,11 +8,83 @@ export class FindOrderReturnData {
   id: string;
 
   @Field()
-  orderDate: string;
+  createdAt: string;
+
+  @Field()
+  updatedAt: string;
 
   @Field()
   orderStatus: OrderStatus;
 
+  @Field(() => Float)
+  orderTotal: number;
+
   @Field(() => [FindOrderOrderItem])
   orderItems: FindOrderOrderItem[];
+
+  @Field()
+  sameBillingAddress: boolean;
+
+  @Field()
+  shippingFirstName: string;
+
+  @Field()
+  shippingLastName: string;
+
+  @Field({ nullable: true })
+  shippingCompanyName: string;
+
+  @Field()
+  shippingAddress: string;
+
+  @Field({ nullable: true })
+  shippingAddressNumber: string;
+
+  @Field()
+  shippingCity: string;
+
+  @Field()
+  shippingCountry: string;
+
+  @Field()
+  shippingZip: string;
+
+  @Field()
+  shippingPhone: string;
+
+  @Field({ nullable: true })
+  billingFirstName: string;
+
+  @Field({ nullable: true })
+  billingLastName: string;
+
+  @Field({ nullable: true })
+  billingCompanyName: string;
+
+  @Field({ nullable: true })
+  billingAddress: string;
+
+  @Field({ nullable: true })
+  billingAddressNumber: string;
+
+  @Field({ nullable: true })
+  billingCity: string;
+
+  @Field({ nullable: true })
+  billingCountry: string;
+
+  @Field({ nullable: true })
+  billingZip: string;
+
+  @Field({ nullable: true })
+  billingPhone: string;
+
+  @Field()
+  creditCardNumber: string;
+
+  @Field()
+  creditCardExpiry: string;
+
+  @Field()
+  creditCardCVC: string;
 }
