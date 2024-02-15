65974aed641dd68254a492c896ae66d1067f72a3
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-15 16:39:10 +0900

    Add validation with class validator

--- src/products/dto/product.args.ts
+++ src/products/dto/product.args.ts
@@ -1,10 +1,13 @@
 import { ArgsType, Field, Int } from '@nestjs/graphql';
+import { IsInt, IsString } from 'class-validator';
 
 @ArgsType()
 export class ProductArgs {
   @Field({ nullable: true })
+  @IsString()
   searchName?: string;
 
   @Field((type) => Int, { nullable: true })
+  @IsInt()
   SKU?: number;
 }
