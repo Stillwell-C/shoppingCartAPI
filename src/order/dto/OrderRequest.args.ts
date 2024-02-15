27d9b6cb6b53a58e99e286b70273afef2f9aaf82
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-15 18:05:06 +0900

    Add class validator validation

--- src/order/dto/OrderRequest.args.ts
+++ src/order/dto/OrderRequest.args.ts
@@ -2,15 +2,27 @@ import { ArgsType, Field, Float } from '@nestjs/graphql';
 import { UserInfo } from './UserInfo';
 import { OrderItem } from './OrderItem';
 import { Prisma } from '@prisma/client';
+import {
+  ArrayNotEmpty,
+  IsArray,
+  IsNumber,
+  IsObject,
+  Min,
+} from 'class-validator';
 
 @ArgsType()
 export class OrderRequest {
   @Field()
+  @IsObject()
   userInfo: UserInfo;
 
   @Field((type) => [OrderItem])
+  @IsArray()
+  @ArrayNotEmpty()
   orderInfo: Prisma.OrderItemCreateManyInput[];
 
   @Field((type) => Float)
+  @IsNumber()
+  @Min(0)
   orderTotal: number;
 }
