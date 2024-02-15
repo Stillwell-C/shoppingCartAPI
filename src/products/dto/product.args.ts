335365a9b49cff166a26e6bd986ca407a1935b69
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-15 17:55:16 +0900

    Update validation for nullable field

--- src/products/dto/product.args.ts
+++ src/products/dto/product.args.ts
@@ -1,13 +1,15 @@
 import { ArgsType, Field, Int } from '@nestjs/graphql';
-import { IsInt, IsString } from 'class-validator';
+import { IsInt, IsString, ValidateIf } from 'class-validator';
 
 @ArgsType()
 export class ProductArgs {
   @Field({ nullable: true })
   @IsString()
+  @ValidateIf((object) => object.hasOwnProperty('searchName'))
   searchName?: string;
 
   @Field((type) => Int, { nullable: true })
   @IsInt()
+  @ValidateIf((object) => object.hasOwnProperty('SKU'))
   SKU?: number;
 }
