b521ea6f878622d4ea3932ad0d8e5e1b65838a33
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-15 16:39:03 +0900

    Add validation with class validator

--- src/products/dto/getProducts.args.ts
+++ src/products/dto/getProducts.args.ts
@@ -1,8 +1,10 @@
 import { ArgsType, Field } from '@nestjs/graphql';
 import { Departments } from '@prisma/client';
+import { IsEnum } from 'class-validator';
 
 @ArgsType()
 export class GetProductsArgs {
   @Field((type) => Departments, { nullable: true })
+  @IsEnum(Departments)
   dept?: Departments;
 }
