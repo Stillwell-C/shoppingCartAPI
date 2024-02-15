aa9d9b0dfa80e6dfb935c88d9d7b1282c35f3a8f
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-15 17:54:08 +0900

    Update validation for nullable field

--- src/products/dto/getProducts.args.ts
+++ src/products/dto/getProducts.args.ts
@@ -1,10 +1,11 @@
 import { ArgsType, Field } from '@nestjs/graphql';
 import { Departments } from '@prisma/client';
-import { IsEnum } from 'class-validator';
+import { IsEnum, ValidateIf } from 'class-validator';
 
 @ArgsType()
 export class GetProductsArgs {
   @Field((type) => Departments, { nullable: true })
+  @ValidateIf((object) => object.hasOwnProperty('dept'))
   @IsEnum(Departments)
-  dept?: Departments;
+  dept?: Departments | null;
 }
