fe27e2aa5d0dfbb4c3847760b25a20c9d5f1181e
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-15 18:16:51 +0900

    Update validation

--- src/mailing-list/dto/create-email.args.ts
+++ src/mailing-list/dto/create-email.args.ts
@@ -1,9 +1,10 @@
 import { Field, ArgsType } from '@nestjs/graphql';
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 @ArgsType()
 export class CreateEmailInput {
   @Field()
   @IsEmail()
+  @IsNotEmpty()
   email: string;
 }
