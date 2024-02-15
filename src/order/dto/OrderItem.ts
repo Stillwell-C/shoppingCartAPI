53474715b4d83a3495b79da260079026c2221d48
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-15 18:09:45 +0900

    Add class validator validation

--- src/order/dto/OrderItem.ts
+++ src/order/dto/OrderItem.ts
@@ -1,13 +1,20 @@
 import { Field, Float, InputType, Int } from '@nestjs/graphql';
+import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 
 @InputType()
 export class OrderItem {
   @Field()
+  @IsString()
+  @IsNotEmpty()
   searchName: string;
 
   @Field((type) => Int)
+  @IsNumber()
+  @Min(0)
   qty: number;
 
   @Field((type) => Float)
+  @IsNumber()
+  @Min(0)
   price: number;
 }
