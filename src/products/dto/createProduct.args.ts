cbca482b1959b03ea6f58b88a21f1d0fe8b1f5cf
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-15 16:38:48 +0900

    Add validation with class validator

--- src/products/dto/createProduct.args.ts
+++ src/products/dto/createProduct.args.ts
@@ -1,6 +1,7 @@
 import { ArgsType, Field, Float, Int, registerEnumType } from '@nestjs/graphql';
 
 import { StockLevels, Departments } from '@prisma/client';
+import { IsEnum, IsInt, IsString, Min } from 'class-validator';
 
 registerEnumType(StockLevels, { name: 'StockLevels' });
 registerEnumType(Departments, { name: 'Departments' });
@@ -8,29 +9,39 @@ registerEnumType(Departments, { name: 'Departments' });
 @ArgsType()
 export class CreateProductArgs {
   @Field((type) => Int)
+  @IsInt()
   SKU: number;
 
   @Field()
+  @IsString()
   name: string;
 
   @Field()
+  @IsString()
   searchName: string;
 
   @Field((type) => Float)
+  @IsInt()
   price: number;
 
   @Field((type) => Departments)
+  @IsEnum(Departments)
   dept: Departments;
 
   @Field()
+  @IsString()
   description: string;
 
   @Field()
+  @IsString()
   img_id: string;
 
   @Field((type) => Int)
+  @IsInt()
   stock: number;
 
   @Field((type) => StockLevels)
+  @Min(0)
+  @IsEnum(Departments)
   stock_level: StockLevels;
 }
