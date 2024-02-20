5f7f4aa3d519d43bba9289d4fd3bc8e69576a94e
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-02-20 13:07:39 +0900

    Update cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const corsOptions = {
+  origin: [
+    'https://shopping-cart-henna-zeta.vercel.app',
+    'http://shopping-cart-henna-zeta.vercel.app',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3500);
 }
